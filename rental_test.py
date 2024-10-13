6e56d7349e43579802551842da6a0a70ed46daa2
Chayakarn Hengsuwan <113860728+colarrbear@users.noreply.github.com>
2024-10-14 01:22:34 +0700

    add test case for rental price

--- rental_test.py
+++ rental_test.py
@@ -17,16 +17,21 @@ class RentalTest(unittest.TestCase):
         self.assertEqual("Air", m.get_title())
         self.assertEqual(Movie.REGULAR, m.get_price_code())
 
-    # @unittest.skip("add this test when you refactor rental price")
     def test_rental_price(self):
         rental = Rental(self.new_movie, 1)
         self.assertEqual(rental.get_price(), 3.0)
         rental = Rental(self.new_movie, 5)
         self.assertEqual(rental.get_price(), 15.0)
+        # regular movie
         rental = Rental(self.regular_movie, 10)
         self.assertEqual(rental.get_price(), 14.0)
+        rental = Rental(self.regular_movie, 2)
+        self.assertEqual(rental.get_price(), 2.0)
+        # childrens movie
         rental = Rental(self.childrens_movie, 10)
         self.assertEqual(rental.get_price(), 12.0)
+        rental = Rental(self.childrens_movie, 3)
+        self.assertEqual(rental.get_price(), 1.5)
 
     @unittest.skip("add this test of rental points when you add it to Rental")
     def test_rental_points(self):
