f1a1afc5dc242acec28e2a36e9b56326127f49a5
Chayakarn Hengsuwan <113860728+colarrbear@users.noreply.github.com>
2024-10-20 17:06:44 +0700

    Define a Factory for Movies

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 # Demonstrate the movie rental code.
 # Create a customer with some movies and print a statement.
 
-from movie import Movie
+from movie import Movie, MovieCatalog
 from rental import Rental
 from customer import Customer
 
 
 def make_movies():
     """Some sample movies."""
+    movie_catalog = MovieCatalog()
     movies = [
-        Movie("Air", Movie.NEW_RELEASE),
-        Movie("Oppenheimer", Movie.REGULAR),
-        Movie("Frozen", Movie.CHILDRENS),
-        Movie("Bitconned", Movie.NEW_RELEASE),
-        Movie("Particle Fever", Movie.REGULAR)
+        # Movie("Air", Movie.NEW_RELEASE),
+        # Movie("Oppenheimer", Movie.REGULAR),
+        # Movie("Frozen", Movie.CHILDRENS),
+        # Movie("Bitconned", Movie.NEW_RELEASE),
+        # Movie("Particle Fever", Movie.REGULAR)
+        movie_catalog.get_movie("Air"),
+        movie_catalog.get_movie("Oppenheimer"),
+        movie_catalog.get_movie("Frozen"),
+        movie_catalog.get_movie("Bitconned"),
+        movie_catalog.get_movie("Particle Fever")
     ]
     return movies
 
 
 if __name__ == '__main__':
     # Create a customer with some rentals
-    customer = Customer("Edward Snowden")
-    days = 1
-    for movie in make_movies():
-        customer.add_rental(Rental(movie, days, movie.price_strategy))
-        days = (days + 2) % 5 + 1
-    print(customer.statement())
+    # customer = Customer("Edward Snowden")
+    # days = 1
+    # for movie in make_movies():
+    #     customer.add_rental(Rental(movie, days, movie.price_strategy))
+    #     days = (days + 2) % 5 + 1
+    # print(customer.statement())
+
+    # to check it get the movie or not
+    # Get the Singleton Movie Catalog
+    catalog = MovieCatalog()
+    # Get the first movie named 'Mulan'
+    # movie = catalog.get_movie("Mulan")
+    # # Get 'Mulan' released in 1998
+    # old_movie = catalog.get_movie("Mulan", 1998)
+    # print(movie)
+    # print(old_movie)
+    movie = catalog.get_movie("No Time to Die")
+    if not movie:
+        print("Sorry, couldn't find that movie.")
+    print(movie)
--- movie.py
+++ movie.py
@@ -1,4 +1,5 @@
-# from pricing import PriceStrategy, NewRelease, RegularPrice, ChildrensPrice
+import csv
+from pricing import NewRelease, RegularPrice, ChildrensPrice
 from typing import Collection
 from dataclasses import dataclass
 
@@ -10,13 +11,54 @@ class Movie:
     """
     title: str
     year: int
-    genre: Collection[str]
+    genres: Collection[str]
 
     def get_title(self) -> str:
         return self.title
 
     def is_genre(self, input_genre: str) -> bool:
-        return input_genre.lower() in self.genre
+        return input_genre.lower() in (genre.lower() for genre in self.genres)
 
     def __str__(self) -> str:
         return f'{self.title} ({self.year})'
+
+
+class MovieCatalog:
+    """
+    A collection of movies.
+    """
+    _instance = None
+    _movies = []
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super(MovieCatalog, cls).__new__(cls)
+            cls._instance._load_movies('movies.csv')
+        return cls._instance
+
+    def _load_movies(self, filename: str):
+        """Load movies from the CSV file."""
+        with open(filename, newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            next(reader)
+            for _, movie_data in enumerate(reader):
+                try:
+                    movie = Movie(
+                        movie_data['title'],
+                        int(movie_data['year']),
+                        movie_data['genres'].split('|')
+                    )
+                    self._movies.append(movie)
+                except (TypeError, ValueError):
+                    continue
+
+            return self._movies
+
+    def get_movie(self, title, year=None):
+        try:
+            for movie in self._movies:
+                if movie.title.lower() == title.lower() and (
+                        year is None or movie.year == year):
+                    return movie
+        except AttributeError:
+            return None
